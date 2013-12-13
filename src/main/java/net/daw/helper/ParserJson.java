8812dfbca7d9f99ab8711d48d583fd6e48491dd3
Álvaro Crego <ereglion18@gmail.com>
2013-12-13 12:46:34 +0100

    mas cosas

---
+++ src/main/java/net/daw/helper/ParserJson.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.daw.helper;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author al037294
+ */
+public class ParserJson {
+    
+    
+    public void parser(){
+        Gson oGson = new Gson();
+    }
+}
